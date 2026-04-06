5169ca64bc094da55484e103e6f27c28b19b32ae
alejandro <wialgica@gmail.com>
2026-04-06 10:13:12 -0500

    implement new component and refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+// AGREGAR ESTA LÍNEA (Asegúrate de que la ruta del archivo sea correcta)
+import { EmpleosComponent } from './components/empleos/empleos.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }, // Ruta principal (la de los perritos)
-  { path: '**', redirectTo: '' } // Si escriben una URL que no existe, los devuelve al inicio
+  { path: '', component: HomeComponent },
+  { path: 'empleos', component: EmpleosComponent }, // Ahora sí sabe qué es EmpleosComponent
+  { path: '**', redirectTo: '' } // Mueve esta línea al final SIEMPRE.
 ];
