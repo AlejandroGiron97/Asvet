2039ddaef33793790d4d9668331dc8abc400cd7e
alejandro <wialgica@gmail.com>
2026-04-09 17:53:52 -0500

    implement refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,20 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { EmpleosComponent } from './components/empleos/empleos.component';
-// 1. Importamos el nuevo componente
-import { EspecializacionesComponent } from './pages/especializaciones/especializaciones.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'empleos', component: EmpleosComponent },
-  { path: 'especializaciones', component: EspecializacionesComponent }, // 2. Nueva ruta independiente
-  { path: '**', redirectTo: '' } // SIEMPRE AL FINAL
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'empleos',
+    loadComponent: () =>
+      import('./components/empleos/empleos.component').then(m => m.EmpleosComponent)
+  },
+  {
+    path: 'especializaciones',
+    loadComponent: () =>
+      import('./pages/especializaciones/especializaciones.component').then(m => m.EspecializacionesComponent)
+  },
+  { path: '**', redirectTo: '' }
 ];
--- src/app/pages/home/experiences/experiences.component.ts
+++ src/app/pages/home/experiences/experiences.component.ts
@@ -17,37 +17,37 @@ export class ExperiencesComponent implements OnInit, OnDestroy {
 
   customerExperiences = [
     {
-      url: '/img/comen_1.png',
+      url: 'img/comen_1.png',
       name: 'Cliente 1',
       comment: 'Excelente servicio, muy recomendado.'
     },
     {
-      url: '/img/comen_2.png',
+      url: 'img/comen_2.png',
       name: 'Cliente 2',
       comment: 'Profesionales y atentos.'
     },
     {
-      url: '/img/comen_3.png',
+      url: 'img/comen_3.png',
       name: 'Cliente 3',
       comment: 'Me encantó la atención.'
     },
     {
-      url: '/img/comen_4.png',
+      url: 'img/comen_4.png',
       name: 'Cliente 4',
       comment: 'Servicio de calidad.'
     },
     {
-      url: '/img/comen_5.png',
+      url: 'img/comen_5.png',
       name: 'Cliente 5',
       comment: 'Muy satisfecho con el cuidado.'
     },
     {
-      url: '/img/comen_6.png',
+      url: 'img/comen_6.png',
       name: 'Cliente 6',
       comment: 'Recomiendo ampliamente.'
     },
     {
-      url: '/img/comen_7.png',
+      url: 'img/comen_7.png',
       name: 'Cliente 7',
       comment: 'Excelente veterinaria.'
     }
