d104760956753802508c3578b0e655ee28ff2f3d
DavidGR0788 <davidgalvezrestrepo@gmail.com>
2026-04-15 22:24:59 -0500

    Addition of slider images and image updates

--- src/app/pages/especializaciones/especializaciones.component.ts
+++ src/app/pages/especializaciones/especializaciones.component.ts
@@ -20,14 +20,14 @@ export class EspecializacionesComponent {
     { titulo: 'Neurología', descripcion: '"Atención especializada en problemas del sistema nervioso, convulsiones y columna vertebral."', imagenUrl: 'img/neuro.webp' },
     { titulo: 'Dermatología', descripcion: '"Cuidado experto para problemas de piel, alergias crónicas y enfermedades del pelaje."', imagenUrl: 'img/derma.webp' },
     { titulo: 'Cardiología', descripcion: '"Diagnóstico y tratamiento de enfermedades del corazón y sistema circulatorio."', imagenUrl: 'img/sinFoto.webp' },
-    { titulo: 'Oftamología', descripcion: '"Cuidado experto para problemas oculares, enfermedades de la visión y afecciones como infecciones, irritaciones y cataratas."', imagenUrl: 'img/sinFoto.webp' },
-    { titulo: 'Oncología', descripcion: '"Diagnóstico y tratamiento del cáncer, incluyendo tumores y su manejo integral."', imagenUrl: 'img/sinFoto.webp' },
-    { titulo: 'Nutrición', descripcion: '"Planificación y control de la alimentación para mantener la salud y el bienestar."', imagenUrl: 'img/sinFoto.webp' },
-    { titulo: 'Nefrología', descripcion: '"Diagnóstico y tratamiento de enfermedades renales y del sistema urinario."', imagenUrl: 'img/sinFoto.webp' },
-    { titulo: 'Etología', descripcion: '"Estudio y manejo del comportamiento animal para mejorar su bienestar y convivencia."', imagenUrl: 'img/etiolo.webp' },
+    { titulo: 'Oftamología', descripcion: '"Cuidado experto para problemas oculares, enfermedades de la visión y afecciones como infecciones, irritaciones y cataratas."', imagenUrl: 'img/ofta.webp' },
+    { titulo: 'Oncología', descripcion: '"Diagnóstico y tratamiento del cáncer, incluyendo tumores y su manejo integral."', imagenUrl: 'img/onco.webp' },
+    { titulo: 'Nutrición', descripcion: '"Planificación y control de la alimentación para mantener la salud y el bienestar."', imagenUrl: 'img/nutri.webp' },
+    { titulo: 'Nefrología', descripcion: '"Diagnóstico y tratamiento de enfermedades renales y del sistema urinario."', imagenUrl: 'img/nefro.webp' },
+    { titulo: 'Etología', descripcion: '"Estudio y manejo del comportamiento animal para mejorar su bienestar y convivencia."', imagenUrl: 'img/etolo.webp' },
     { titulo: 'Gastroenterología', descripcion: '"Diagnóstico y tratamiento de enfermedades del sistema digestivo."', imagenUrl: 'img/gastro.webp' },
     { titulo: 'Medicina Interna', descripcion: '"Diagnóstico y tratamiento integral de enfermedades internas."', imagenUrl: 'img/sinFoto.webp' },
-    { titulo: 'Odontología', descripcion: '"Cuidado y tratamiento de la salud dental, incluyendo limpieza, prevención y enfermedades bucales."', imagenUrl: 'img/sinFoto.webp' },
+    { titulo: 'Odontología', descripcion: '"Cuidado y tratamiento de la salud dental, incluyendo limpieza, prevención y enfermedades bucales."', imagenUrl: 'img/denta.webp' },
   ];
 
   // 🔥 LA MISMA LÓGICA DEL MODAL (¡Cero esfuerzo extra!) 🔥
